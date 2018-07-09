1f3e52efbd0e8211706f4b9462fb4afc851d6d7a
Deepak singh rawat <deepak.rawat@spicedigital.in>
2018-07-09 11:53:01 +0530

    Initial Commit

---
+++ IBDesignables/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBDesignables
+//
+//  Created by admin on 09/07/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
